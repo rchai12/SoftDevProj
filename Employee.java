2fb96ab30b597259d339c4178ceb806909b80b40
Roman Johnson <125155563+stormcloud20@users.noreply.github.com>
2025-04-17 12:51:45 -0400

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,49 @@
+public class Employee {
+    public int employeeId;
+    public String firstName;
+    public String lastName;
+    public String SSN;
+    public String jobTitle;
+    public String division;
+    public double salary;
+
+    public Employee(int employeeId, String firstName, String lastName, String SSN, String jobTitle, String division, double salary) {
+        this.employeeId = employeeId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.SSN = SSN;
+        this.jobTitle = jobTitle;
+        this.division = division;
+        this.salary = salary;
+    }
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public String getDivision() {
+        return division;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public String getJobTitle(){
+        return jobTitle;
+    }
+    public String getSSN(){
+        return SSN;
+    }
+    public double getSalary(){
+        return salary;
+    }
+    public double updateSalary(double newSalary){
+        return newSalary;
+    }
+
+}
+
+
---
+++ UserInput.java
@@ -0,0 +1,100 @@
+import java.util.Scanner;
+public class UserInput {
+    public Employee getEmployeeInfo() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter ID: ");
+        int id = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println("Enter First Name: ");
+        String first_name = scan.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        String last_name = scan.nextLine();
+
+        System.out.println("Enter your SSN: ");
+        String SSN = scan.nextLine();
+
+        System.out.println("Enter your Job Title: ");
+        String job_title = scan.nextLine();
+
+        System.out.println("Entre your division: ");
+        String division = scan.nextLine();
+
+        System.out.println("What is your monthly salary: ");
+        double salary = scan.nextDouble();
+
+        return new Employee(id, first_name, last_name, SSN, job_title, division, salary);
+    }
+
+    public void getSearchParameters() {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Searching for User? Enter the employees full name, SSN, and / or Employee ID. Leave field blank to skip. \n Enter First Name: ");
+        String first_name = scan.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        String last_name = scan.nextLine();
+
+        System.out.println("Enter the SSN: ");
+        String SSN = scan.nextLine();
+
+        System.out.println("Enter ID: ");
+        int id = scan.nextInt();
+        scan.nextLine();
+    }
+
+    public Employee updateEmployeeFromInput(Employee employee) {
+        Scanner scan = new Scanner(System.in);
+        Employee updatedEmployee = new Employee(employee.employeeId, employee.firstName, employee.lastName, employee.SSN, employee.division, employee.jobTitle, employee.salary);
+
+        boolean done = false;
+        while (!done) {
+            System.out.println("\nSelect field to update:");
+            System.out.println("1. First Name");
+            System.out.println("2. Last Name");
+            System.out.println("3. SSN");
+            System.out.println("4. Job Title");
+            System.out.println("5. Division");
+            System.out.println("6. Salary");
+            System.out.println("7. Done");
+
+            System.out.print("Enter the number of the field you wish to change: ");
+            int choice = Integer.parseInt(scan.nextLine());
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the new First Name: ");
+                    updatedEmployee.firstName = scan.nextLine();
+                    break;
+                case 2:
+                    System.out.println("Enter the new Last Name: ");
+                    updatedEmployee.lastName = scan.nextLine();
+                    break;
+                case 3:
+                    System.out.println("Enter the new SSN: ");
+                    updatedEmployee.SSN = scan.nextLine();
+                    break;
+                case 4:
+                    System.out.println("Enter the new job title: ");
+                    updatedEmployee.jobTitle = scan.nextLine();
+                    break;
+                case 5:
+                    System.out.println("Enter the new Division: ");
+                    updatedEmployee.division = scan.nextLine();
+                    break;
+                case 6:
+                    System.out.println("Enter your new Salary: ");
+                    updatedEmployee.salary = scan.nextDouble();
+                    break;
+                case 7:
+                    done = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again!");
+            }
+        }
+        return updatedEmployee;
+    }
+}
